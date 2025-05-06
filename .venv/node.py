2c01bdacec9ffd6bf1f5e3f093c5d98f36783a2c
Kopal <kopal@ucr-secure-33-10-13-182-110.wnet.ucr.edu>
2025-05-05 20:11:28 -0700

    add node class

---
+++ .venv/node.py
@@ -0,0 +1,155 @@
+import math
+import copy
+
+class Node:
+    goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]] 
+    goal_positions = {}
+
+    for row in range(3):
+        for col in range(3):
+            if goal_state[row][col] != 0:
+                goal_positions[goal_state[row][col]] = (row, col)
+
+    def __init__(self, state, parent=None, action=None, g_cost=0, heuristic='manhattan'):
+        self.state = state
+        # Use tuple representation for hashing and set membership checks
+        self.state_tuple = tuple(map(tuple, self.state))
+        self.parent = parent
+        self.action = action
+        self.g_cost = g_cost
+        self.heuristic = heuristic
+        self.h_cost = self.calculate_heuristic()
+        self.f_cost = self.g_cost + self.h_cost
+
+    def calculate_heuristic(self):
+        if self.heuristic == 'uniform':
+            return 0
+        elif self.heuristic == 'misplaced':
+            return self._misplaced_tiles()
+        elif self.heuristic == 'manhattan':
+            return self._manhattan_distance()
+
+    def _misplaced_tiles(self):
+        misplaced = 0
+        for row in range(3):
+            for col in range(3):
+                current_tile = self.state[row][col]
+                if current_tile != 0 and current_tile != Node.goal_state[row][col]:
+                    misplaced += 1
+        return misplaced
+
+    def _manhattan_distance(self):
+        for row in range(3):
+            for col in range(3):
+                current_tile = self.state[row][col]
+                if current_tile != 0:
+                    goal_row, goal_col = Node.goal_positions[current_tile]
+                    d_row = abs(row - goal_row)
+                    d_col = abs(col - goal_col)
+        return d_row + d_col
+
+    def find_blank(self):
+        for r in range(3):
+            for c in range(3):
+                if self.state[r][c] == 0:
+                    return r, c
+        return None # Should not happen in a valid 8-puzzle
+
+    def generate_children(self):
+        children = []
+        blank_row, blank_col = self.find_blank()
+
+        if blank_row > 0: # Move UP
+            new_state = copy.deepcopy(self.state)
+            swap_row, swap_col = blank_row - 1, blank_col
+            
+            temp = new_state[blank_row][blank_col]
+            new_state[blank_row][blank_col] = new_state[swap_row][swap_col]
+            new_state[swap_row][swap_col] = temp
+
+            child_node = Node(state=new_state,
+                              parent=self,
+                              action='up', # Action taken was 'up'
+                              g_cost=self.g_cost + 1, # Cost increases by 1 move
+                              heuristic=self.heuristic)
+
+            children.append(child_node) # Add to the list of children
+
+        if blank_row < 2: # Move DOWN
+            new_state = copy.deepcopy(self.state)
+            swap_row, swap_col = blank_row + 1, blank_col
+
+            temp = new_state[blank_row][blank_col]
+            new_state[blank_row][blank_col] = new_state[swap_row][swap_col]
+            new_state[swap_row][swap_col] = temp
+            
+            child_node = Node(state=new_state,
+                              parent=self,
+                              action='down',
+                              g_cost=self.g_cost + 1,
+                              heuristic=self.heuristic)
+            children.append(child_node)
+
+        if blank_col > 0: # Move LEFT
+            new_state = copy.deepcopy(self.state)
+            swap_row, swap_col = blank_row, blank_col - 1
+
+            temp = new_state[blank_row][blank_col]
+            new_state[blank_row][blank_col] = new_state[swap_row][swap_col]
+            new_state[swap_row][swap_col] = temp
+
+            child_node = Node(state=new_state,
+                              parent=self,
+                              action='left',
+                              g_cost=self.g_cost + 1,
+                              heuristic=self.heuristic)
+
+            children.append(child_node)
+
+        if blank_col < 2: # Move RIGHT
+            new_state = copy.deepcopy(self.state)
+            swap_row, swap_col = blank_row, blank_col + 1
+
+            temp = new_state[blank_row][blank_col]
+            new_state[blank_row][blank_col] = new_state[swap_row][swap_col]
+            new_state[swap_row][swap_col] = temp
+            
+            child_node = Node(state=new_state,
+                              parent=self,
+                              action='right',
+                              g_cost=self.g_cost + 1,
+                              heuristic=self.heuristic)
+
+            children.append(child_node)
+
+        return children # Return valid children 
+
+    def get_path(self):
+        path = []
+        current = self
+
+        while current:
+            path.insert(0, (current.action, current.state))
+            current = current.parent
+        return path[1:] # Remove the initial state's None action
+
+    # --- Methods for Priority Queue and Set Operations ---
+
+    def __lt__(self, other):
+        """Comparison for priority queue (less than). Prioritizes lower f_cost."""
+        return self.f_cost < other.f_cost
+
+    def __eq__(self, other):
+        """Equality check based on state."""
+        if not isinstance(other, Node):
+            return NotImplemented
+        return self.state_tuple == other.state_tuple
+
+    def __hash__(self):
+        """Hash based on the state tuple for use in sets."""
+        return hash(self.state_tuple)
+
+    def __repr__(self):
+        """String representation for debugging."""
+        return f"Node(f={self.f_cost}, g={self.g_cost}, h={self.h_cost}, state={self.state})"
+
