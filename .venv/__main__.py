9b22ec68deb6ba3695d332b0f9e6cf7acfa93a8c
Kopal <kopal@ucr-secure-33-10-13-180-252.wnet.ucr.edu>
2025-05-08 15:36:09 -0700

    Add: comments and code generalization

--- .venv/__main__.py
+++ .venv/__main__.py
@@ -1,18 +1,23 @@
 import heapq
 import time
 import sys
+import math
 from node import Node 
 
-def is_solvable(initial_state): # Check if the puzzle is solvable
+# Check if the puzzle is solvable. 
+# An 8-puzzle problem is solvable as long as the number of inversions is even
+# For an N-puzzle problem, this function can be modified with a modicum of effort to check for even/odd number of inversions
+def is_solvable(initial_state): 
     inversions = 0
     tiles = [tile for tile in initial_state if tile != 0]
     n = len(tiles)
     for i in range(n):
         for j in range(i + 1, n):
             if tiles[i] > tiles[j]:
-                inversions += 1
-    return inversions % 2 == 0
+                inversions += 1 # Increase the inversion count if the successor tile  < predecessor tile 
+    return inversions % 2 == 0  # Return true if inversion count is even
 
+# This function takes the user input
 def get_user_input():
 
     print("Enter the initial state of the 8-puzzle on a 3 x 3 grid ")
@@ -22,20 +27,26 @@ def get_user_input():
 
     initial_state = []
 
-    for i in range(3):
-        row = i + 1
-        row_input = input(f"\nEnter row {row}: ").strip().split()
+    N = 8 # N is the number of tiles
+    n = int(math.sqrt(N + 1)) # Calculate the dimension of the grid
 
-        row = [int(x) for x in row_input]
-        initial_state.append(row)
+    for i in range(n):
+        row = i + 1 
+        # Process the user input
+        # Remove any erroneous blank spaces at the end using strip
+        # Split the space separated user input into distinct list elements e.g  1 2 3 -> 1, 2, 3
+        row_input = input(f"\nEnter row {row}: ").strip().split() 
 
+        row = [int(x) for x in row_input] 
+        initial_state.append(row) # Append the elements/row to the initial_state list
 
+    # Check for solvable state. Abort execution if the state is not solvable
     state = [tile for row in initial_state for tile in row]
     if not is_solvable(state):
         print("\nThe provided puzzle is unsolvable. Aborting execution.")
         sys.exit(1)  # error
     
-    # Select heuristic
+    # Input heuristic and map to the correct algorithm 
     print("\nSelect the search heuristic:")
     print("1. Uniform Cost Search")
     print("2. A* with Misplaced Tile Heuristic")
@@ -47,7 +58,7 @@ def get_user_input():
 
     return initial_state, heuristic_map[heuristic]
 
-
+# This function handles printing the "state" of the puzzle
 def print_state(state):
     print("-" * 20)
     for row in state:
@@ -56,56 +67,72 @@ def print_state(state):
             print(f" {tile if tile != 0 else ' '} ", end="|")
         print("\n" + "-" * 20)
 
+# A * search function
+# Takes the initial state and the selected heuristic as input
 def a_star_search(initial_state, heuristic):
-    start_time = time.time()
+    start_time = time.time() # Time when the search starts, used to calculate time taken by search
 
-    root_node = Node(state=initial_state, heuristic=heuristic)
+    root_node = Node(state=initial_state, heuristic=heuristic) # Initialize the root/first node by calling the Node class
 
-    if root_node.state_tuple == Node.goal_state_tuple:
+    if root_node.state_tuple == Node.goal_state_tuple: # Check if the initial state is the goal state. The puzzle is already solved.
         print("Initial state is the goal state")
-        return [], 0, 1, 0 # Path, expanded, max_queue, depth
+        return [], 0, 1, 0 # return Path, expanded_nodes, max_queue_size, solution_depth
 
+    # This is our frontier. Use a heap to keep the node with the lowest f_cost at the front
+    # We store tuples of (f_cost, node) so the heap sorts by f_cost (min heap). First node is with the lowest f_cost.
     nodes = [(root_node.f_cost, root_node)]
-    heapq.heapify(nodes)
 
+    # Turn the list into a heap
+    heapq.heapify(nodes) 
+
+    # Track the nodes we've visited and the min cost (g cost) to reach that node
     visited = {root_node.state_tuple: root_node.g_cost}
 
+    # Initialize counters to track the metrics
     nodes_expanded = 0
     max_queue_size = 1
 
+    # Loop while the frontier is not empty
     while nodes:
-        max_queue_size = max(max_queue_size, len(nodes))
-        f_cost, current_node = heapq.heappop(nodes)
+        max_queue_size = max(max_queue_size, len(nodes)) # Compare and update the max queue size
+        f_cost, current_node = heapq.heappop(nodes)      # Pop the next best node to explore, lowest f_cost node
 
+        # If the current state is the goal state, Stop the search. Return metrics and path
         if current_node.state_tuple == Node.goal_state_tuple:
-            end_time = time.time()
+            end_time = time.time() 
             path = current_node.get_path()
             depth = len(path)
             print(f"\n Goal reached in {end_time - start_time:.4f} seconds\n")
             return path, nodes_expanded, max_queue_size, depth, end_time - start_time
-
+        
+        # Else, continue to search by expanding the nodes/looking at its neighbours
         nodes_expanded += 1
 
-        children = current_node.generate_children()
+        # Find all the possible next states/children from the current node. The function is defined in the Node class
+        children = current_node.generate_children() 
 
+        # Iterate over every child node
         for child in children:
             child_state_tuple = child.state_tuple
             child_g_cost = child.g_cost
 
-            # If the child state has not been visited OR
-            # If the child state has been visited, but this new path (via current_node) is shorter (lower g_cost) than the previously found path
+            # If we have not visited the child state so far OR
+            # The path cost (g) to get to the child node is cheaper via the current path, record the g_cost
             if child_state_tuple not in visited or child_g_cost < visited[child_state_tuple]:
                 visited[child_state_tuple] = child_g_cost
-                heapq.heappush(nodes, (child.f_cost, child))
-
-    end_time = time.time()
+                heapq.heappush(nodes, (child.f_cost, child)) # Add the child to the queue 
+    
+    # Loop ends when the frontier is empty.
+    end_time = time.time()  # No solution found, stop the timer and end search
     print(f"No solution found after {end_time - start_time:.4f} sec. Search failed.")
     return None, nodes_expanded, max_queue_size, 0, end_time - start_time
 
 
+# Driver code
 if __name__ == "__main__":
 
-    initial_state, heuristic = get_user_input()
+    initial_state, heuristic = get_user_input() # Call the user input function
+
 
     if initial_state and heuristic:
         print("\n Initial State:")
@@ -117,8 +144,10 @@ if __name__ == "__main__":
         print(f"\nBeginning search using {heuristic}: \n")
         print("*" * 20)
 
+        # Begin the a-star seach 
         solution_path, nodes_expanded, max_queue_size, depth, time_taken = a_star_search(initial_state, heuristic)
 
+        # Print the solution path and metrics if a solution exists and the path isn't empty
         if solution_path is not None:
             print("*" * 20)
             print(f" * Nodes Expanded: {nodes_expanded}")
--- .venv/node.py
+++ .venv/node.py
@@ -1,75 +1,95 @@
 import math
 import copy
 
+# This class represents a single state/arragnement in the puzzle search
+# Each node knows its state, path, and its cost
+
+N = 8 # Number of tiles
+n = int(math.sqrt(N + 1)) # Grid size. Depends on the value of N
+
 class Node:
-    goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]] 
+    goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]] # Default goal state
     goal_positions = {}
 
+    # Make the goal state into a tuple of tuples. 
+    # Tuples are immutable. Python needs to make things 'hashable' before we can use them for comparision
     goal_state_tuple = tuple(map(tuple, goal_state))
 
-    for row in range(3):
-        for col in range(3):
+    # Figure out the goal position for each tile, store in a dictionary
+    for row in range(n):
+        for col in range(n):
             if goal_state[row][col] != 0:
                 goal_positions[goal_state[row][col]] = (row, col)
 
+    # Initialize/Create a new Node object
     def __init__(self, state, parent=None, action=None, g_cost=0, heuristic='manhattan'):
         self.state = state
 
-        self.state_tuple = tuple(map(tuple, self.state))
-        self.parent = parent
-        self.action = action
-        self.g_cost = g_cost
-        self.heuristic = heuristic
-        self.h_cost = self.calculate_heuristic()
-        self.f_cost = self.g_cost + self.h_cost
+        self.state_tuple = tuple(map(tuple, self.state)) # Make the current state into a tuple of tuples. 
+        self.parent = parent                             # The node that we came from to get to this state
+        self.action = action                             # The action that got us to this state
+        self.g_cost = g_cost                             # Path cost to get to this state
+        self.heuristic = heuristic                       # Heuristic to use as selected by the user
+        self.h_cost = self.calculate_heuristic()         # Calculate the h_cost 
+        self.f_cost = self.g_cost + self.h_cost          # Calculate f_cost 
 
+
+    # Function to calculate the h_cost
     def calculate_heuristic(self):
-        if self.heuristic == 'uniform':
+        if self.heuristic == 'uniform': # Uniform cost search is like A* with h=0
             return 0
-        elif self.heuristic == 'misplaced':
+        elif self.heuristic == 'misplaced': # Call the function
             return self._misplaced_tiles()
         elif self.heuristic == 'manhattan':
-            return self._manhattan_distance()
+            return self._manhattan_distance() # Call the function
 
+    # Calculate h_cost for misplaced tile heuristic
     def _misplaced_tiles(self):
         misplaced = 0
-        for row in range(3):
-            for col in range(3):
+        # Iterate through every spot in the grid
+        for row in range(n):
+            for col in range(n):
                 current_tile = self.state[row][col]
+                # If the current tile is not the blank AND not in the right place, increment count by 1
                 if current_tile != 0 and current_tile != Node.goal_state[row][col]:
                     misplaced += 1
-        return misplaced
+        return misplaced # Return the total count of misplaced tiles for the current state
 
     def _manhattan_distance(self):
         total = 0
-        for row in range(3):
-            for col in range(3):
+        # Iterate through every spot in the grid
+        for row in range(n):
+            for col in range(n):
                 current_tile = self.state[row][col]
+                # If the current tile is not the blank, calculate the manhattan distance from the goal state
                 if current_tile != 0:
-                    goal_row, goal_col = Node.goal_positions[current_tile]
-                    total += abs(row - goal_row) + abs(col - goal_col)
-        return total
+                    goal_row, goal_col = Node.goal_positions[current_tile] # Get the actual/expected coordinates of the current tile
+                    total += abs(row - goal_row) + abs(col - goal_col)     # Calulate the distance betwwen the present and expected coordinates
+        return total # Repeat for all tiles in the grid and return the total manhattan distance for the current state
     
-
+    # Find the coordinates of the blank tile
     def find_blank(self):
-        for r in range(3):
-            for c in range(3):
+        for r in range(n):
+            for c in range(n):
                 if self.state[r][c] == 0:
-                    return r, c
-        return None # Should not happen in a valid 8-puzzle
-
+                    return r, c # Return its row and column
+        return None 
+    
+    # Generate all possible next states/children nodes from the current state
     def generate_children(self):
         children = []
-        blank_row, blank_col = self.find_blank()
+        blank_row, blank_col = self.find_blank() # Find where the blank tile is
 
         if blank_row > 0: # Move UP
-            new_state = copy.deepcopy(self.state)
-            swap_row, swap_col = blank_row - 1, blank_col
+            new_state = copy.deepcopy(self.state) # Make a copy of the current state and modigy it to get the new state
+            swap_row, swap_col = blank_row - 1, blank_col # Swap with the tile directly above the blank space
             
+            # Swap tiles
             temp = new_state[blank_row][blank_col]
             new_state[blank_row][blank_col] = new_state[swap_row][swap_col]
             new_state[swap_row][swap_col] = temp
 
+            # Create a new Node for this new state 
             node = Node(state=new_state,
                               parent=self,
                               action='up', # Action taken was 'up'
@@ -79,64 +99,75 @@ class Node:
             children.append(node) # Add to the list of children
 
         if blank_row < 2: # Move DOWN
-            new_state = copy.deepcopy(self.state)
-            swap_row, swap_col = blank_row + 1, blank_col
+            new_state = copy.deepcopy(self.state) # Make a copy of the current state and modigy it to get the new state
+            swap_row, swap_col = blank_row + 1, blank_col # Swap with the tile directly below the blank space
 
+            # Swap tiles
             temp = new_state[blank_row][blank_col]
             new_state[blank_row][blank_col] = new_state[swap_row][swap_col]
             new_state[swap_row][swap_col] = temp
             
+            # Create a new Node for this new state 
             node = Node(state=new_state,
                               parent=self,
                               action='down',
                               g_cost=self.g_cost + 1,
                               heuristic=self.heuristic)
-            children.append(node)
+            
+            children.append(node) # Add to the list of children
 
         if blank_col > 0: # Move LEFT
-            new_state = copy.deepcopy(self.state)
-            swap_row, swap_col = blank_row, blank_col - 1
+            new_state = copy.deepcopy(self.state)  # Make a copy of the current state and modigy it to get the new state
+            swap_row, swap_col = blank_row, blank_col - 1 # Swap with the tile directly left of the blank space
 
+            # Swap tiles
             temp = new_state[blank_row][blank_col]
             new_state[blank_row][blank_col] = new_state[swap_row][swap_col]
             new_state[swap_row][swap_col] = temp
 
+            # Create a new Node for this new state 
             node = Node(state=new_state,
                               parent=self,
                               action='left',
                               g_cost=self.g_cost + 1,
                               heuristic=self.heuristic)
 
-            children.append(node)
+            children.append(node) # Add to the list of children
 
         if blank_col < 2: # Move RIGHT
-            new_state = copy.deepcopy(self.state)
-            swap_row, swap_col = blank_row, blank_col + 1
+            new_state = copy.deepcopy(self.state) # Make a copy of the current state and modigy it to get the new state
+            swap_row, swap_col = blank_row, blank_col + 1 # Swap with the tile directly right of the blank space
 
+            # Swap tiles
             temp = new_state[blank_row][blank_col]
             new_state[blank_row][blank_col] = new_state[swap_row][swap_col]
             new_state[swap_row][swap_col] = temp
             
+            # Create a new Node for this new state 
             node = Node(state=new_state,
                               parent=self,
                               action='right',
                               g_cost=self.g_cost + 1,
                               heuristic=self.heuristic)
 
-            children.append(node)
+            children.append(node) # Add to the list of children
 
         return children # Return valid children 
+
+    # Reconstruct the path from the initial state to the current node
 
     def get_path(self):
         path = []
         current = self
 
+        # Keep looping until we reach the root node (with no parents)
         while current:
-            path.insert(0, (current.action, current.state))
-            current = current.parent
-        return path[1:] # Remove the initial state's None action
+            path.insert(0, (current.action, current.state)) # Add the action that led to this node and the current state to the list
+            current = current.parent 
+        return path[1:] # Remove the (None, initial_state) element for the intital state node
 
-
+    # Used by the Python heapq priority queue to sort the nodes
+    # Nodes with lower f_cost are considered more important that nodes with higher f_cost
     def __lt__(self, child):
         if self.f_cost != child.f_cost:
             return self.f_cost < child.f_cost
